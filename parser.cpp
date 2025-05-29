312b507191a7186381f2f19acc0840c98027ef17
Cryplo <dylanliaa@gmail.com>
2025-05-28 21:40:43 -0400

    add support for ls and pwd, need to restructure file names and code

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include "builtins.h"
 #include <unistd.h>
+#include <sys/wait.h>
 
 //FSM for parsing?
 void parse(std::vector<Token> tokens, bool* alive){
@@ -20,6 +21,27 @@ void parse(std::vector<Token> tokens, bool* alive){
             //change this later, testing for now
             else if(token.value == "ls"){
                 pid_t c_pid = fork();
+                // Is a child process?
+                if(c_pid == 0){
+                    execvp("ls", new char*[2]{const_cast<char*>("ls"), NULL});
+                    exit(0);
+                }
+                // Is a parent process?
+                else{
+                    waitpid(c_pid, NULL, 0);
+                }
+            }
+            else if(token.value == "pwd"){
+                pid_t c_pid = fork();
+                // Is a child process?
+                if(c_pid == 0){
+                    execvp("pwd", new char* [2]{const_cast<char*>("pwd"), NULL});
+                    exit(0);
+                }
+                // Is a parent process?
+                else{
+                    waitpid(c_pid, NULL, 0);
+                }
             }
             else{
                 unknown(token);
