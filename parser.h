e9e07bf0923da1e61e70e9ce7e03bcce266b111d
Cryplo <dylanliaa@gmail.com>
2025-05-30 19:29:55 -0400

    Add operator and string nodes

--- parser.h
+++ parser.h
@@ -29,20 +29,65 @@ struct Node{
 struct CommandNode : Node{
     std::string cmd;
     std::vector<std::string> args;
-    CommandNode(std::string cmd,
-                std::vector<std::string> args){
+    CommandNode(
+        std::string cmd,
+        std::vector<std::string> args
+    ){
         this->cmd = cmd;
         this->args = args;
     }
-    CommandNode(Node* child1,
-                Node* child2,
-                std::string cmd,
-                std::vector<std::string> args){
+    CommandNode(
+        Node* child1,
+        Node* child2,
+        std::string cmd,
+        std::vector<std::string> args
+    ){
         Node(child1, child2);
         this->cmd = cmd;
         this->args = args;
     }
+    std::string getCommand() return cmd;
+    std::vector<std::string> getArgs() return args;
 };
+
+struct OperatorNode : Node{
+    std::string operator;
+    OperatorNode(
+        std::string operator
+    ){
+        this->operator = operator;
+    }
+    OperatorNode(
+        Node* child1,
+        Node* child2,
+        std::string operator
+    ){
+        this->child1 = child1;
+        this->child2 = child2;
+        this->operator = operator;
+    }
+    std::string getOperator() return operator;
+}
+
+//Not CMD / Operator
+struct StringNode : Node{
+    std::string str;
+    StringNode(
+        std::string str
+    ){
+        this->str = str;
+    }
+    StringNode(
+        Node* child1,
+        Node* child2,
+        std::string str
+    ){
+        this->child1 = child1;
+        this->child2 = child2;
+        this->str = str;
+    }
+    std::string getString() return str;
+}
 
 extern std::set<std::string> cmds;
 
