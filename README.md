e5f88d6914d68b745a035871e3a0449d08581d15
Cryplo <dylanliaa@gmail.com>
2025-05-31 22:49:44 -0400

    Start implementing arrow press to retrieve shell history (imported conio.h implementation for posix, as it is written only for dos). not working

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ What I've learned from ScrappyShell so far
 - Basic C++ Project Stuff
 - Refreshed overall C++ knowledge
 - How shell interacts with OS
+- UNIX commands
 - Setting up with MakeFiles
 - Setting up WSL
 - Language inferencing, i.e lexing and parsing
 - Forking processes, and process IDs, etc. (remember, in child process fork returns 0, in parent process fork returns child ID)
 - File I/O, i.e buffers
+- Multithreading
--- main.cpp
+++ main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 #include <string>
 #include <utility>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include "conio.h"
 #include "lexer.h"
 #include "parser.h"
 
-int main(){
+void keyPressThread(std::vector<std::string> &inputs, std::vector<std::string>::iterator &inputIterator, std::mutex &mtx){
+    while(true){
+        switch(getch()){
+            case 72:
+                mtx.lock();
+                if(inputIterator != inputs.begin()) --inputIterator;
+                std::cout << "\r" << *inputIterator << std::flush;
+                mtx.unlock();
+                break;
+            case 80:
+                mtx.lock();
+                if(inputIterator != inputs.end()-1) ++inputIterator;
+                std::cout << "\r" << *inputIterator << std::flush;
+                mtx.unlock();
+                break;
+            default:
+                break;
+        }
+    }
+}
 
+int main(){
     // INIT shell
     std::string input;
     bool alive = true;
 
+    std::vector<std::string> inputs;
+    inputs.push_back("dsfaw");
+    //use a mutex to prevent keypressthread from altering
+    std::vector<std::string>::iterator inputIterator;
+    std::mutex mtx;
+    //Can probably use non-blocking input to detect keypresses, but I want to implement multithreading
+    std::thread t(keyPressThread, std::ref(inputs), std::ref(inputIterator), std::ref(mtx));
+    t.detach();
     while(alive){
         std::cout << "ScrappyShell> ";
         std::getline(std::cin, input);
         parse(tokenize(input), (&alive));
-        /*parse*/
-        /*
-        for(std::pair<TokenType, std::string> p : tokenize(input)){
-            std::cout << p.second << std::endl;
-        }*/
+        inputs.push_back(input);
+        mtx.lock();
+        inputIterator = inputs.end() - 1;
+        mtx.unlock();
     }
 
 }
