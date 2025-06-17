2631f08ec213b34d1b2dd33c21bd41c4ce5d0710
Cryplo <dylanliaa@gmail.com>
2025-06-16 22:04:03 -0400

    cleanup builtins (small)

--- builtins.cpp
+++ builtins.cpp
@@ -1,23 +1,5 @@
-#include <iostream>
 #include "builtins.h"
-
-void echo(std::string output){
-    std::cout << output << std::endl;
-}
 
 void quit(bool* alive){
     (*alive) = false;
-}
-
-void unknownString(std::string str){
-    std::cout << str << " is not recognized. Cancelling." << std::endl;
-}
-
-//TODO: Suggest maybe there was a typo?
-void unknownToken(Token token){
-    std::cout << "The " << (token.type == TokenType::WORD ? "WORD " : "SYMBOL ") << token.value << " is not recognized." << std::endl;
-}
-
-void unknownCommand(std::string str){
-    std::cout << "The command " << str << " is not recognized." << std::endl;
 }
--- builtins.h
+++ builtins.h
@@ -1,17 +1,6 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
-#include <string>
-#include "lexer.h"
-
-void echo(std::string output);
-
 void quit(bool* alive);
-
-void unknownString(std::string str);
-
-void unknownToken(Token token);
-
-void unknownCommand(std::string str);
 
 #endif
